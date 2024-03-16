7b35690fe07035363cf48776a7eae9672de99eb8
rehmnjr <rehmnfahim@gmail.com>
2024-03-16 14:50:42 +0530

    creating_nav&footer_components_1

---
+++ components/footer.js
@@ -0,0 +1,147 @@
+function footer(){
+    return ` <footer>
+    <div class="footer-body">
+        <div class="footer-content ">
+            <div class="cont-div1 cont1">
+                <ul>
+                    <a href="">
+                        <li class="list">
+                            <h4> ONLINE SHOPPING</h4>
+                        </li>
+                    </a>
+                    <br>
+                    <a href="">
+                        <li class="list"> Men </li>
+                    </a>
+                    <a href="">
+                        <li class="list"> Women </li>
+                    </a>
+                    <a href="">
+                        <li class="list"> Kids </li>
+                    </a>
+                    <a href="">
+                        <li class="list"> Home & Living </li>
+                    </a>
+                    <a href="">
+                        <li class="list"> Beauty </li>
+                    </a>
+                    <a href="">
+                        <li class="list"> Gift Cards </li>
+                    </a>
+                    <a href="">
+                        <li class="list"> Myntra Insider </li>
+                    </a>
+                    <br>
+                    <a href="">
+                        <li class="list">
+                            <h4>USEFUL LINKS</h4>
+                        </li>
+                    </a>
+                    <br>
+                    <a href="">
+                        <li class="list"> </li>
+                    </a>
+                    <a href="">
+                        <li class="list"> Blog </li>
+                    </a>
+                    <a href="">
+                        <li class="list"> Careers </li>
+                    </a>
+                    <a href="">
+                        <li class="list"> Site Map </li>
+                    </a>
+                    <a href="">
+                        <li class="list"> Corporate Information </li>
+                    </a>
+                    <a href="">
+                        <li class="list"> Whitehat </li>
+                    </a>
+                    <a href="">
+                        <li class="list"> Cleartrip </li>
+                    </a>
+                </ul>
+            </div>
+
+            <div class="cont-div2 cont1">
+                <ul>
+                    <a href="">
+                        <li class="list">
+                            <h4> CUSTOMER POLICIES</h4>
+                        </li>
+                    </a>
+                    <br>
+                    <a href="">
+                        <li class="list"> Contact Us </li>
+                    </a>
+                    <a href="">
+                        <li class="list"> FAQ </li>
+                    </a>
+                    <a href="">
+                        <li class="list"> T&C </li>
+                    </a>
+                    <a href="">
+                        <li class="list"> Terms Of Use </li>
+                    </a>
+                    <a href="">
+                        <li class="list"> Track Orders </li>
+                    </a>
+                    <a href="">
+                        <li class="list"> Shipping </li>
+                    </a>
+                    <a href="">
+                        <li class="list"> Cancellation </li>
+                    </a>
+                    <a href="">
+                        <li class="list"> Returns </li>
+                    </a>
+                    <a href="">
+                        <li class="list"> Privacy policy </li>
+                    </a>
+                    <a href="">
+                        <li class="list"> Grievance Officer </li>
+                    </a>
+                </ul>
+            </div>
+
+            <div class="cont-div3">
+
+                <h4> EXPERIENCE MYNTRA APP ON MOBILE </h4>
+                <div class="playstore">
+                    <a href="">
+                        <img src="https://constant.myntassets.com/web/assets/img/80cc455a-92d2-4b5c-a038-7da0d92af33f1539674178924-google_play.png"
+                            alt="">
+                    </a>
+                    <a href="">
+                        <img src="https://constant.myntassets.com/web/assets/img/bc5e11ad-0250-420a-ac71-115a57ca35d51539674178941-apple_store.png"
+                            alt="">
+                    </a>
+                </div>
+                <h4> KEEP IN TOUCH </h4>
+                <div class="social-icon">
+                    <a href="">
+                        <img src="https://constant.myntassets.com/web/assets/img/d2bec182-bef5-4fab-ade0-034d21ec82e31574604275433-fb.png"
+                            alt="">
+                    </a>
+                    <a href="">
+                        <img src="https://constant.myntassets.com/web/assets/img/f10bc513-c5a4-490c-9a9c-eb7a3cc8252b1574604275383-twitter.png"
+                            alt="">
+                    </a>
+                    <a href="">
+                        <img src="https://constant.myntassets.com/web/assets/img/a7e3c86e-566a-44a6-a733-179389dd87111574604275355-yt.png"
+                            alt="">
+                    </a>
+                    <a href="">
+                        <img src="https://constant.myntassets.com/web/assets/img/b4fcca19-5fc1-4199-93ca-4cae3210ef7f1574604275408-insta.png"
+                            alt="">
+                    </a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</footer>
+    
+    
+    `
+}
+export default footer;
--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+
 document.addEventListener('DOMContentLoaded', function () {
+    let fade = document.querySelector('.fade-box');
+
     let a = document.querySelector('.men-category-hide');
     let menBox = document.querySelector('.box1');
 
@@ -19,58 +22,70 @@ document.addEventListener('DOMContentLoaded', function () {
 
     menBox.addEventListener('mouseover', function () {
         a.classList.remove('hide');
+        fade.classList.remove('fade-hide');
     });
 
    menBox.addEventListener('mouseout', function () {
         a.classList.add('hide');
+        fade.classList.add('fade-hide');
     });  
 
 
      womenBox.addEventListener('mouseover', function () {
         b.classList.remove('hide2');
+        fade.classList.remove('fade-hide');
     });
 
    womenBox.addEventListener('mouseout', function () {
         b.classList.add('hide2');
+        fade.classList.add('fade-hide');
 
     });   
 
 
     kidBox.addEventListener('mouseover', function () {
         c.classList.remove('hide3');
+        fade.classList.remove('fade-hide');
     });
 
    kidBox.addEventListener('mouseout', function () {
     c.classList.add('hide3');
+    fade.classList.add('fade-hide');
         
     });   
 
     homeBox.addEventListener('mouseover', function () {
         d.classList.remove('hide4');
+        fade.classList.remove('fade-hide');
     });
 
    homeBox.addEventListener('mouseout', function () {
     d.classList.add('hide4');
+    fade.classList.add('fade-hide');
         
     });   
 
 
     beautyBox.addEventListener('mouseover', function () {
         e.classList.remove('hide5');
+        fade.classList.remove('fade-hide');
     });
 
    beautyBox.addEventListener('mouseout', function () {
     e.classList.add('hide5');
+    fade.classList.add('fade-hide');
         
     }); 
     
     
     studioBox.addEventListener('mouseover', function () {
         f.classList.remove('hide6');
+        fade.classList.remove('fade-hide');
     });
 
    studioBox.addEventListener('mouseout', function () {
     f.classList.add('hide6');
+    fade.classList.add('fade-hide');
         
     }); 
 
