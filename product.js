4532753d5b679dd68ef30cdd23bf96f019d73dc9
rehmnjr <rehmnfahim@gmail.com>
2024-04-22 23:32:36 +0530

    product_page_1

---
+++ product.js
@@ -0,0 +1,498 @@
+
+let products = [
+  
+  {
+    "Product Name": "Heels",
+    "Price": 1399.00,
+    "Quantity": 5488,
+    "Size": 8,
+    "Color": "Silver",
+    "Brand Name": "Zambia",
+    "Gender": "Women",
+    "Image URL": "https://example.com/cotton-tshirt.jpg"
+  },
+  {
+    "Product Name": "Flats",
+    "Price": 799.00,
+    "Quantity": 3265,
+    "Size": 6,
+    "Color": "white",
+    "Brand Name": "Niggas",
+    "Gender": "Unisex",
+    "Image URL": "https://example.com/cotton-tshirt.jpg"
+  },
+  {
+    "Product Name": "Sport Shoe",
+    "Price": 699.00,
+    "Quantity": 1956,
+    "Size": 8,
+    "Color": "Black",
+    "Brand Name": "Jumps",
+    "Gender": "Unisex",
+    "Image URL": "https://example.com/cotton-tshirt.jpg"
+  },
+
+  {
+    "Product Name": "FLip Flops",
+    "Price": 499.00,
+    "Quantity": 8516,
+    "Size": 5,
+    "Color": "white",
+    "Brand Name": "Flite",
+    "Gender": "Unisex",
+    "Image URL": "https://example.com/cotton-tshirt.jpg"
+  },
+  {
+    "Product Name": "Formal Shoes",
+    "Price": 2599.00,
+    "Quantity": 6515,
+    "Size": 7,
+    "Color": "Brown",
+    "Brand Name": "Levis",
+    "Gender": "Men",
+    "Image URL": "https://example.com/cotton-tshirt.jpg"
+  },
+
+  {
+    "Product Name": "Sandals",
+    "Price": 199.00,
+    "Quantity": 3150,
+    "Size": 9,
+    "Color": "Black",
+    "Brand Name": "Bata",
+    "Gender": "Unisex",
+    "Image URL": "https://example.com/cotton-tshirt.jpg"
+  },
+
+  
+  {
+    "Product Name": "Casual Shoe",
+    "Price": 1999.99,
+    "Quantity": 4652,
+    "Size": 5,
+    "Color": "white",
+    "Brand Name": "ComfyCloth",
+    "Gender": "Unisex",
+    "Image URL": "https://example.com/cotton-tshirt.jpg"
+  },
+
+    {
+      "Product Name": "Cotton T-Shirt",
+      "Price": 459.00,
+      "Quantity": 5265,
+      "Size": "M",
+      "Color": "Red",
+      "Brand Name": "Tommy Smith",
+      "Gender": "Unisex",
+      "Image URL": "https://example.com/cotton-tshirt.jpg"
+    },
+
+    {
+      "Product Name": "Running Sneakers",
+      "Price": 3999.00,
+      "Quantity": 6548,
+      "Size": 10,
+      "Color": "Black",
+      "Brand Name": "QuickStep",
+      "Gender": "Men",
+      "Image URL": "https://example.com/cotton-tshirt.jpg"
+    },
+    {
+      "Product Name": "Leather Wallet",
+      "Price": 899.00,
+      "Quantity": 545,
+      "Size": "One Size",
+      "Color": "Brown",
+      "Brand Name": "LuxeLeather",
+      "Gender": "Men",
+      "Image URL": "https://example.com/cotton-tshirt.jpg"
+    },
+    {
+      "Product Name": "Silk Scarf",
+      "Price": 799.00,
+      "Quantity": 4898,
+      "Size": "One Size",
+      "Color": "Blue",
+      "Brand Name": "SilkySmooth",
+      "Gender": "Women",
+      "Image URL": "https://example.com/cotton-tshirt.jpg"
+    },
+    {
+      "Product Name": "Sports Watch",
+      "Price": 1299.00,
+      "Quantity": 8784,
+      "Size": "One Size",
+      "Color": "Black",
+      "Brand Name": "TimeRunner",
+      "Gender": "Unisex",
+      "Image URL": "https://example.com/cotton-tshirt.jpg"
+    },
+    {
+      "Product Name": "Denim Jeans",
+      "Price": 599.00,
+      "Quantity": 548,
+      "Size": "L",
+      "Color": "Navy",
+      "Brand Name": "DenimWorks",
+      "Gender": "Women",
+      "Image URL": "https://example.com/cotton-tshirt.jpg"
+    },
+    {
+      "Product Name": "Canvas Belt",
+      "Price": 4899.00,
+      "Quantity": 484,
+      "Size": "L",
+      "Color": "Grey",
+      "Brand Name": "DurableWear",
+      "Gender": "Unisex",
+      "Image URL": "https://example.com/cotton-tshirt.jpg"
+    },
+    {
+      "Product Name": "Yoga Pants",
+      "Price": 39.99,
+      "Quantity": 70,
+      "Size": "S",
+      "Color": "Purple",
+      "Brand Name": "FlexiFit",
+      "Gender": "Women",
+      "Image URL": "https://example.com/cotton-tshirt.jpg"
+    },
+    {
+      "Product Name": "Woolen Cap",
+      "Price": 24.99,
+      "Quantity": 90,
+      "Size": "One Size",
+      "Color": "Maroon",
+      "Brand Name": "WarmHead",
+      "Gender": "Unisex",
+      "Image URL": "https://example.com/cotton-tshirt.jpg"
+    },
+    {
+      "Product Name": "Suede Shoes",
+      "Price": 99.99,
+      "Quantity": 55,
+      "Size": 9,
+      "Color": "Brown",
+      "Brand Name": "ClassicStep",
+      "Gender": "Men",
+      "Image URL": "https://example.com/cotton-tshirt.jpg"
+    },
+    {
+      "Product Name": "Graphic Sweatshirt",
+      "Price": 1599.00,
+      "Quantity": 6525,
+      "Size": "XL",
+      "Color": "Green",
+      "Brand Name": "TrendTees",
+      "Gender": "Unisex",
+      "Image URL": "https://example.com/cotton-tshirt.jpg"
+    },
+    {
+      "Product Name": "Pleated Skirt",
+      "Price": 2199.00,
+      "Quantity": 60,
+      "Size": "M",
+      "Color": "Pink",
+      "Brand Name": "ChicStyle",
+      "Gender": "Women",
+      "Image URL": "https://example.com/cotton-tshirt.jpg"
+    },
+    {
+      "Product Name": "Mesh Running Shorts",
+      "Price": 3999.00,
+      "Quantity": 5485,
+      "Size": "L",
+      "Color": "Black",
+      "Brand Name": "SpeedSport",
+      "Gender": "Men",
+      "Image URL": "https://example.com/cotton-tshirt.jpg"
+    },
+    {
+      "Product Name": "Leather Jacket",
+      "Price": 4999.00,
+      "Quantity": 4887,
+      "Size": "M",
+      "Color": "Black",
+      "Brand Name": "RoadRogue",
+      "Gender": "Unisex",
+      "Image URL": "https://example.com/cotton-tshirt.jpg"
+    },
+    {
+      "Product Name": "Sling Bag",
+      "Price": 3299.00,
+      "Quantity": 1651,
+      "Size": "One Size",
+      "Color": "Coral",
+      "Brand Name": "UrbanMove",
+      "Gender": "Women",
+      "Image URL": "https://example.com/cotton-tshirt.jpg"
+    },
+    {
+      "Product Name": "Chelsea Boots",
+      "Price": 1299.00,
+      "Quantity": 4115,
+      "Size": 11,
+      "Color": "Grey",
+      "Brand Name": "SturdyStep",
+      "Gender": "Men",
+      "Image URL": "https://example.com/cotton-tshirt.jpg"
+    },
+    {
+      "Product Name": "Maxi Dress",
+      "Price": 79.99,
+      "Quantity": 50,
+      "Size": "L",
+      "Color": "Violet",
+      "Brand Name": "ElegantWear",
+      "Gender": "Women",
+      "Image URL": "https://example.com/cotton-tshirt.jpg"
+    },
+    {
+      "Product Name": "Athletic Tank Top",
+      "Price": 25.99,
+      "Quantity": 100,
+      "Size": "M",
+      "Color": "White",
+      "Brand Name": "PeakPerform",
+      "Gender": "Men",
+      "Image URL": "https://example.com/cotton-tshirt.jpg"
+    },
+    {
+      "Product Name": "Cropped Hoodie",
+      "Price": 42.99,
+      "Quantity": 80,
+      "Size": "S",
+      "Color": "Yellow",
+      "Brand Name": "YouthVogue",
+      "Gender": "Women",
+      "Image URL": "https://example.com/cotton-tshirt.jpg"
+    },
+    {
+      "Product Name": "Smartwatch",
+      "Price": 159.99,
+      "Quantity": 50,
+      "Size": "One Size",
+      "Color": "Silver",
+      "Brand Name": "TechTrend",
+      "Gender": "Unisex",
+      "Image URL": "https://example.com/cotton-tshirt.jpg"
+    },
+
+      {
+        "Product Name": "Cotton T-Shirt",
+        "Price": 19.99,
+        "Quantity": 100,
+        "Size": "M",
+        "Color": "Red",
+        "Brand Name": "ComfyCloth",
+        "Gender": "Unisex",
+        "Image URL": "https://example.com/cotton-tshirt.jpg"
+      },
+      {
+        "Product Name": "Running Sneakers",
+        "Price": 89.99,
+        "Quantity": 50,
+        "Size": 10,
+        "Color": "Black",
+        "Brand Name": "QuickStep",
+        "Gender": "Men",
+        "Image URL": "https://example.com/running-sneakers.jpg"
+      },
+      {
+        "Product Name": "Leather Wallet",
+        "Price": 49.99,
+        "Quantity": 75,
+        "Size": "One Size",
+        "Color": "Brown",
+        "Brand Name": "LuxeLeather",
+        "Gender": "Men",
+        "Image URL": "https://example.com/leather-wallet.jpg"
+      },
+      {
+        "Product Name": "Silk Scarf",
+        "Price": 35.99,
+        "Quantity": 30,
+        "Size": "One Size",
+        "Color": "Blue",
+        "Brand Name": "SilkySmooth",
+        "Gender": "Women",
+        "Image URL": "https://example.com/silk-scarf.jpg"
+      },
+      {
+        "Product Name": "Sports Watch",
+        "Price": 129.99,
+        "Quantity": 40,
+        "Size": "One Size",
+        "Color": "Black",
+        "Brand Name": "TimeRunner",
+        "Gender": "Unisex",
+        "Image URL": "https://example.com/sports-watch.jpg"
+      },
+      {
+        "Product Name": "Denim Jeans",
+        "Price": 59.99,
+        "Quantity": 60,
+        "Size": "L",
+        "Color": "Navy",
+        "Brand Name": "DenimWorks",
+        "Gender": "Women",
+        "Image URL": "https://example.com/denim-jeans.jpg"
+      },
+      {
+        "Product Name": "Canvas Belt",
+        "Price": 15.99,
+        "Quantity": 85,
+        "Size": "L",
+        "Color": "Grey",
+        "Brand Name": "DurableWear",
+        "Gender": "Unisex",
+        "Image URL": "https://example.com/canvas-belt.jpg"
+      },
+      {
+        "Product Name": "Yoga Pants",
+        "Price": 39.99,
+        "Quantity": 70,
+        "Size": "S",
+        "Color": "Purple",
+        "Brand Name": "FlexiFit",
+        "Gender": "Women",
+        "Image URL": "https://example.com/yoga-pants.jpg"
+      },
+      {
+        "Product Name": "Woolen Cap",
+        "Price": 24.99,
+        "Quantity": 90,
+        "Size": "One Size",
+        "Color": "Maroon",
+        "Brand Name": "WarmHead",
+        "Gender": "Unisex",
+        "Image URL": "https://example.com/woolen-cap.jpg"
+      },
+      {
+        "Product Name": "Suede Shoes",
+        "Price": 99.99,
+        "Quantity": 55,
+        "Size": 9,
+        "Color": "Brown",
+        "Brand Name": "ClassicStep",
+        "Gender": "Men",
+        "Image URL": "https://example.com/suede-shoes.jpg"
+      },
+      {
+        "Product Name": "Graphic Sweatshirt",
+        "Price": 44.99,
+        "Quantity": 80,
+        "Size": "XL",
+        "Color": "Green",
+        "Brand Name": "TrendTees",
+        "Gender": "Unisex",
+        "Image URL": "https://example.com/graphic-sweatshirt.jpg"
+      },
+      {
+        "Product Name": "Pleated Skirt",
+        "Price": 29.99,
+        "Quantity": 60,
+        "Size": "M",
+        "Color": "Pink",
+        "Brand Name": "ChicStyle",
+        "Gender": "Women",
+        "Image URL": "https://example.com/pleated-skirt.jpg"
+      },
+      {
+        "Product Name": "Mesh Running Shorts",
+        "Price": 22.99,
+        "Quantity": 50,
+        "Size": "L",
+        "Color": "Black",
+        "Brand Name": "SpeedSport",
+        "Gender": "Men",
+        "Image URL": "https://example.com/mesh-running-shorts.jpg"
+      },
+      {
+        "Product Name": "Leather Jacket",
+        "Price": 199.99,
+        "Quantity": 40,
+        "Size": "M",
+        "Color": "Black",
+        "Brand Name": "RoadRogue",
+        "Gender": "Unisex",
+        "Image URL": "https://example.com/leather-jacket.jpg"
+      },
+      {
+        "Product Name": "Sling Bag",
+        "Price": 32.99,
+        "Quantity": 65,
+        "Size": "One Size",
+        "Color": "Coral",
+        "Brand Name": "UrbanMove",
+        "Gender": "Women",
+        "Image URL": "https://example.com/sling-bag.jpg"
+      },
+      {
+        "Product Name": "Chelsea Boots",
+        "Price": 120.99,
+        "Quantity": 45,
+        "Size": 11,
+        "Color": "Grey",
+        "Brand Name": "SturdyStep",
+        "Gender": "Men",
+        "Image URL": "https://example.com/chelsea-boots.jpg"
+      },
+      {
+        "Product Name": "Maxi Dress",
+        "Price": 79.99,
+        "Quantity": 50,
+        "Size": "L",
+        "Color": "Violet",
+        "Brand Name": "ElegantWear",
+        "Gender": "Women",
+        "Image URL": "https://example.com/maxi-dress.jpg"
+      },
+      {
+        "Product Name": "Athletic Tank Top",
+        "Price": 25.99,
+        "Quantity": 100,
+        "Size": "M",
+        "Color": "White",
+        "Brand Name": "PeakPerform",
+        "Gender": "Men",
+        "Image URL": "https://example.com/athletic-tank-top.jpg"
+      },
+      {
+        "Product Name": "Cropped Hoodie",
+        "Price": 42.99,
+        "Quantity": 80,
+        "Size": "S",
+        "Color": "Yellow",
+        "Brand Name": "YouthVogue",
+        "Gender": "Women",
+        "Image URL": "https://example.com/cropped-hoodie.jpg"
+      },
+      {
+        "Product Name": "Smartwatch",
+        "Price": 159.99,
+        "Quantity": 870,
+        "Size": "S",
+        "Color": "Yellow",
+        "Brand Name": "YouthVogue",
+        "Gender": "Women",
+        "Image URL": "https://example.com/cropped-hoodie.jpg"
+      }
+  ];
+
+
+  let product = document.querySelector('.product-list');
+
+
+
+products.forEach(i =>{
+    let element = 'li';
+    let list = document.createElement(`${element}`);
+    list.innerHTML = ` <input type="checkbox" id="${i["Product Name"]}">
+    <label for="${i["Product Name"]}">${i["Product Name"]}
+    <span class="quantity">(${i["Quantity"]})</span>`;
+
+    product.appendChild(list);
+
+});
+
