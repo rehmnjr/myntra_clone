eb1dbb713e1755aa538be52749f528a1b19d7b2b
rehmnjr <rehmnfahim@gmail.com>
2024-04-23 23:58:53 +0530

    product_page_update2

--- product.js
+++ product.js
@@ -9,6 +9,7 @@ let products = [
     "Color": "Silver",
     "Brand Name": "Zambia",
     "Gender": "Women",
+    "Discount": 80,
     "Image URL": "https://images-na.ssl-images-amazon.com/images/I/3163f2F46WL._SL500_._AC_SL500_.jpg"
   },
   {
@@ -19,6 +20,7 @@ let products = [
     "Color": "white",
     "Brand Name": "Niggas",
     "Gender": "Unisex",
+    "Discount": 45,
     "Image URL": "https://images.meesho.com/images/products/263304483/wptys_512.jpg"
   },
   {
@@ -29,6 +31,7 @@ let products = [
     "Color": "Black",
     "Brand Name": "Jumps",
     "Gender": "Unisex",
+    "Discount": 45,
     "Image URL": "https://rukminim2.flixcart.com/image/850/1000/k6l2vm80/kids-shoe/k/y/m/6-fashon-14-clark-creation-original-imafpyg6h5w8hdg4.jpeg?q=20&crop=false"
   },
 
@@ -40,6 +43,7 @@ let products = [
     "Color": "white",
     "Brand Name": "Flite",
     "Gender": "Unisex",
+    "Discount": 80,
     "Image URL": "https://m.media-amazon.com/images/I/61oXMd+h45L._AC_UY1000_.jpg"
   },
   {
@@ -50,6 +54,7 @@ let products = [
     "Color": "Brown",
     "Brand Name": "Levis",
     "Gender": "Men",
+    "Discount": 80,
     "Image URL": "https://www.zoomshoes.in/cdn/shop/products/9_1.jpg?v=1619594466"
   },
 
@@ -61,6 +66,7 @@ let products = [
     "Color": "Black",
     "Brand Name": "Bata",
     "Gender": "Unisex",
+    "Discount": 40,
     "Image URL": "https://www.jiomart.com/images/product/original/rvmosllqd4/paragon-k6019l-women-sandals-casual-sandals-stylish-comfortable-durable-for-daily-wear-product-images-rvmosllqd4-0-202309061251.jpg?im=Resize=(500,630)"
   },
 
@@ -73,6 +79,7 @@ let products = [
     "Color": "white",
     "Brand Name": "ComfyCloth",
     "Gender": "Unisex",
+    "Discount": 40,
     "Image URL": "https://img0.junaroad.com/uiproducts/18736906/zoom_0-1695868110.jpg"
   },
 
@@ -84,6 +91,7 @@ let products = [
       "Color": "Red",
       "Brand Name": "Tommy Smith",
       "Gender": "Unisex",
+      "Discount": 30,
       "Image URL": "https://5.imimg.com/data5/SELLER/Default/2021/11/JC/TY/PT/49387708/160-gsm-pure-cotton-t-shirt-500x500.jpg"
     },
 
@@ -95,6 +103,7 @@ let products = [
       "Color": "Black",
       "Brand Name": "QuickStep",
       "Gender": "Men",
+      "Discount": 20,
       "Image URL": "https://www.jiomart.com/images/product/original/rvhulsgnbh/puma-men-s-radcliff-puma-black-sports-running-shoe-product-images-rvhulsgnbh-0-202210212016.jpg?im=Resize=(500,630)"
     },
     {
@@ -105,6 +114,7 @@ let products = [
       "Color": "Brown",
       "Brand Name": "LuxeLeather",
       "Gender": "Men",
+      "Discount": 80,
       "Image URL": "https://5.imimg.com/data5/YM/KV/FW/SELLER-11897351/men-leather-wallet.jpg"
     },
     {
@@ -115,6 +125,7 @@ let products = [
       "Color": "Blue",
       "Brand Name": "SilkySmooth",
       "Gender": "Women",
+      "Discount": 60,
       "Image URL": "https://5.imimg.com/data5/SELLER/Default/2022/5/CY/TE/FV/65727/daily-wear-vintage-lady-silk-scarf-500x500.jpg"
     },
     {
@@ -125,6 +136,7 @@ let products = [
       "Color": "Black",
       "Brand Name": "TimeRunner",
       "Gender": "Unisex",
+      "Discount": 30,
       "Image URL": "https://www.carlington.in/cdn/shop/products/16-e_6cfd0706-a7ac-4060-92db-11bc912a35ef.jpg?v=1694958559&width=2700"
     },
     {
@@ -135,6 +147,7 @@ let products = [
       "Color": "Navy",
       "Brand Name": "DenimWorks",
       "Gender": "Women",
+      "Discount": 40,
       "Image URL": "https://www.mumkins.in/cdn/shop/products/jeans-for-boys-bl06466c-lightblue-1.jpg?v=1662548635&width=1080"
     },
     {
@@ -145,6 +158,7 @@ let products = [
       "Color": "Grey",
       "Brand Name": "DurableWear",
       "Gender": "Unisex",
+      "Discount": 50,
       "Image URL": "https://images-cdn.ubuy.co.in/6538fb4c4e88cc13af6b9453-itiezy-mens-canvas-belts-cloth-fabric.jpg"
     },
     {
@@ -155,6 +169,7 @@ let products = [
       "Color": "Purple",
       "Brand Name": "FlexiFit",
       "Gender": "Women",
+      "Discount": 90,
       "Image URL": "https://akns-images.eonline.com/eol_images/Entire_Site/2019323/rs_1920x1423-190423130254-1920-lululemon.jpg?fit=around%7C1920:1423&output-quality=90&crop=1920:1423;center,top"
     },
     {
@@ -165,6 +180,7 @@ let products = [
       "Color": "Maroon",
       "Brand Name": "WarmHead",
       "Gender": "Unisex",
+      "Discount": 80,
       "Image URL": "https://m.media-amazon.com/images/I/71eNMhvT9VL._AC_UY1100_.jpg"
     },
     {
@@ -175,6 +191,7 @@ let products = [
       "Color": "Brown",
       "Brand Name": "ClassicStep",
       "Gender": "Men",
+      "Discount": 80,
       "Image URL": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhzxC10Sv9BSy6h4IM5GXZA2kr2VwHkuM14cFGVcFkzg&s"
     },
     {
@@ -185,6 +202,7 @@ let products = [
       "Color": "Green",
       "Brand Name": "TrendTees",
       "Gender": "Unisex",
+      "Discount": 60,
       "Image URL": "https://assets.ajio.com/medias/sys_master/root/20231028/xyXM/653c208cddf77915195f78d4/-473Wx593H-466751571-white-MODEL.jpg"
     },
     {
@@ -195,6 +213,7 @@ let products = [
       "Color": "Pink",
       "Brand Name": "ChicStyle",
       "Gender": "Women",
+      "Discount": 20,
       "Image URL": "https://images-cdn.ubuy.co.in/656f963c84ea591576496b7b-girls-women-high-waisted-pleated-skirt.jpg"
     },
     {
@@ -205,6 +224,7 @@ let products = [
       "Color": "Black",
       "Brand Name": "SpeedSport",
       "Gender": "Men",
+      "Discount": 55,
       "Image URL": "https://m.media-amazon.com/images/I/81HLeS7MZ-L._AC_UY1100_.jpg"
     },
     {
@@ -215,6 +235,7 @@ let products = [
       "Color": "Black",
       "Brand Name": "RoadRogue",
       "Gender": "Unisex",
+      "Discount": 50,
       "Image URL": "https://images.bestsellerclothing.in/data/JJ/12-oct-2023/258373101_g0.jpg?width=1080&height=1355&mode=fill&fill=blur&format=auto"
     },
     {
@@ -225,6 +246,7 @@ let products = [
       "Color": "Coral",
       "Brand Name": "UrbanMove",
       "Gender": "Women",
+      "Discount": 17,
       "Image URL": "https://zouk.co.in/cdn/shop/files/ikatzoukodop-05.png?v=1690466200&width=3126"
     },
     {
@@ -235,6 +257,7 @@ let products = [
       "Color": "Grey",
       "Brand Name": "SturdyStep",
       "Gender": "Men",
+      "Discount": 35,
       "Image URL": "https://5.imimg.com/data5/SELLER/Default/2023/5/306689886/QG/CY/TQ/8852934/-mg-7936-500x500.JPG"
     },
     {
@@ -245,6 +268,7 @@ let products = [
       "Color": "Violet",
       "Brand Name": "ElegantWear",
       "Gender": "Women",
+      "Discount": 10,
       "Image URL": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_McbUyLhkBQ5el2Zx_EJM3oYW3ZehumTGJQiRRemP2w&s"
     },
     {
@@ -255,6 +279,7 @@ let products = [
       "Color": "White",
       "Brand Name": "PeakPerform",
       "Gender": "Men",
+      "Discount": 20,
       "Image URL": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUBLfcSNsfpTnkgFNsUpsHrf_9ZKGzPbMScDqJgklB7A&s"
     },
     {
@@ -265,6 +290,7 @@ let products = [
       "Color": "Yellow",
       "Brand Name": "YouthVogue",
       "Gender": "Women",
+      "Discount": 33,
       "Image URL": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNFKzg6EdgyHunM4BDa_VrzLYPks1A1Vi89gp04_3qvg&s"
     },
     {
@@ -275,6 +301,7 @@ let products = [
       "Color": "Silver",
       "Brand Name": "TechTrend",
       "Gender": "Unisex",
+      "Discount": 60,
       "Image URL": "https://5.imimg.com/data5/SELLER/Default/2022/8/JM/UB/XN/128815866/watch.jpg"
     },
 
@@ -286,6 +313,7 @@ let products = [
         "Color": "Red",
         "Brand Name": "ComfyCloth",
         "Gender": "Unisex",
+        "Discount": 45,
         "Image URL": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRajqgtJMfnyApxE7RjCsQOJxt-LunksJ18JJzmx2LocQ&s"
       },
       {
@@ -296,6 +324,7 @@ let products = [
         "Color": "Black",
         "Brand Name": "QuickStep",
         "Gender": "Men",
+        "Discount": 12,
         "Image URL": "https://m.media-amazon.com/images/I/61o6MQ1y4jL._AC_UY1000_.jpg"
       },
       {
@@ -306,6 +335,7 @@ let products = [
         "Color": "Brown",
         "Brand Name": "LuxeLeather",
         "Gender": "Men",
+        "Discount": 23,
         "Image URL": "https://m.media-amazon.com/images/I/81PSyHWGKkL._AC_UY1100_.jpg"
       },
       {
@@ -316,6 +346,7 @@ let products = [
         "Color": "Blue",
         "Brand Name": "SilkySmooth",
         "Gender": "Women",
+        "Discount": 75,
         "Image URL": "https://m.media-amazon.com/images/I/71F2JB0KbiL._AC_UY1100_.jpg"
       },
       {
@@ -326,6 +357,7 @@ let products = [
         "Color": "Black",
         "Brand Name": "TimeRunner",
         "Gender": "Unisex",
+        "Discount": 70,
         "Image URL": "https://m.media-amazon.com/images/I/61i-k76LoQL._AC_UY1000_.jpg"
       },
       {
@@ -336,6 +368,7 @@ let products = [
         "Color": "Navy",
         "Brand Name": "DenimWorks",
         "Gender": "Women",
+        "Discount": 20,
         "Image URL": "https://5.imimg.com/data5/SELLER/Default/2023/6/313423300/MK/WD/CG/40698162/henly-square-jeans.jpeg"
       },
       {
@@ -346,6 +379,7 @@ let products = [
         "Color": "Grey",
         "Brand Name": "DurableWear",
         "Gender": "Unisex",
+        "Discount": 30,
         "Image URL": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLWi3d5SBRngj_oY1Pw597s0Vv2ytgNGD8t89v2HBmKg&s"
       },
       {
@@ -356,6 +390,7 @@ let products = [
         "Color": "Purple",
         "Brand Name": "FlexiFit",
         "Gender": "Women",
+        "Discount": 80,
         "Image URL": "https://www.shape.com/thmb/GVOReIADA6e2DLMgs6lzndFHt60=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/yoga-square-4f5bb270d75a424eab0a25487322996f.jpg"
       },
       {
@@ -366,6 +401,7 @@ let products = [
         "Color": "Maroon",
         "Brand Name": "WarmHead",
         "Gender": "Unisex",
+        "Discount": 45,
         "Image URL": "https://m.media-amazon.com/images/I/51gA8AddOWL._AC_UY1100_.jpg"
       },
       {
@@ -376,6 +412,7 @@ let products = [
         "Color": "Brown",
         "Brand Name": "ClassicStep",
         "Gender": "Men",
+        "Discount": 55,
         "Image URL": "https://i.pinimg.com/736x/42/af/68/42af688d6110ec76c3d1481a56adecff.jpg"
       },
       {
@@ -386,6 +423,7 @@ let products = [
         "Color": "Green",
         "Brand Name": "TrendTees",
         "Gender": "Unisex",
+        "Discount": 10,
         "Image URL": "https://images-cdn.ubuy.co.in/633ff39c01f80f6c8c68abc1-love-square-design-cute-unicorn-dab.jpg"
       },
       {
@@ -396,6 +434,7 @@ let products = [
         "Color": "Pink",
         "Brand Name": "ChicStyle",
         "Gender": "Women",
+        "Discount": 30,
         "Image URL": "https://i.pinimg.com/736x/83/de/39/83de3966cf45f564a62e1bd1611cfc23.jpg"
       },
       {
@@ -406,6 +445,7 @@ let products = [
         "Color": "Black",
         "Brand Name": "SpeedSport",
         "Gender": "Men",
+        "Discount": 55,
         "Image URL": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrmRzlK2svu24jqtowcT0njoxeO6aR6fTbSAb4zc0cfw&s"
       },
       {
@@ -416,6 +456,7 @@ let products = [
         "Color": "Black",
         "Brand Name": "RoadRogue",
         "Gender": "Unisex",
+        "Discount": 75,
         "Image URL": "https://chersdelights.com/cdn/shop/products/KKJJ001-front-800x_1200x.jpg?v=1701966226"
       },
       {
@@ -426,6 +467,7 @@ let products = [
         "Color": "Coral",
         "Brand Name": "UrbanMove",
         "Gender": "Women",
+        "Discount": 40,
         "Image URL": "https://4.imimg.com/data4/IG/YD/MY-35744774/sling-bag-500x500.jpg"
       },
       {
@@ -436,6 +478,7 @@ let products = [
         "Color": "Grey",
         "Brand Name": "SturdyStep",
         "Gender": "Men",
+        "Discount": 40,
         "Image URL": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpF6AtlOjUTOPDXVjgh6_uWdnfQiy3MR2M7s_zS_FvCw&s"
       },
       {
@@ -446,6 +489,7 @@ let products = [
         "Color": "Violet",
         "Brand Name": "ElegantWear",
         "Gender": "Women",
+        "Discount": 90,
         "Image URL": "https://images-cdn.ubuy.co.in/635eeb17698bed130a32e863-smarat-long-sleeve-square-neck-dress.jpg"
       },
       {
@@ -456,6 +500,7 @@ let products = [
         "Color": "White",
         "Brand Name": "PeakPerform",
         "Gender": "Men",
+        "Discount": 50,
         "Image URL": "https://image.made-in-china.com/202f0j00wZmQKGasrPYN/Racerback-Tank-Tops-for-Women-Workout-Tops-Sleeveless-Athletic-Tank-Top-Yoga-Wear-Esg16132.webp"
       },
       {
@@ -466,6 +511,7 @@ let products = [
         "Color": "Yellow",
         "Brand Name": "YouthVogue",
         "Gender": "Women",
+        "Discount": 70,
         "Image URL": "https://www.johnelliott.com/cdn/shop/products/230108_REPLICA_OVERSIZED_CROPPED_HOODIE_BLACK_01.jpg?v=1676131920"
       },
       {
@@ -476,6 +522,7 @@ let products = [
         "Color": "Yellow",
         "Brand Name": "YouthVogue",
         "Gender": "Women",
+        "Discount": 80,
         "Image URL": "https://www.fastrack.in/dw/image/v2/BKDD_PRD/on/demandware.static/-/Sites-titan-master-catalog/default/dw6494ff3a/images/Fastrack/Catalog/38103AM04_1.jpg?sw=800&sh=800"
       }
   ];
@@ -497,9 +544,11 @@ products.forEach(i =>{
     let proImage = document.createElement('div');
     proImage.className = "sub-image";
     proImage.innerHTML = `  <img src=${i["Image URL"]}>
+    <div class="detail">
     <h3>${i["Brand Name"]}</h3>
-    <span class="price">${i["Product Name"]}</span>
-    <span class="price">Rs. ${i["Price"]}</span> `
+    <span class="product-name"> ${i["Color"]} ${i["Product Name"]} for ${i["Gender"]}</span>
+    <span class="price">Rs. ${i["Price"]} <strike class="strike-price ">Rs. ${ Math.round(((i["Price"]*100)/i["Discount"])*100)/100}</strike> <span class="per-cent">(${i["Discount"]}% OFF)</span> </span></span>
+    </div> `
     product_image.appendChild(proImage);
 
 });
